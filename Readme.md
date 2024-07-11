b2c5bfe5684f82aef9042d92fbf12ad759d25098
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-07-11 10:06:57 -0400

    commit userAuth

--- Readme.md
+++ Readme.md
@@ -5,3 +5,8 @@ This is my Auth Service architecture
 Step1: Output(GET: current/users/currentuser)
 ![alt img](./imgs/step1:authserver1.png)
 ![alt img](./imgs/step1:authserver2.png)
+
+step2: Auth signup architecture -> error checks and connection to mongo DB(Kubernetes Pod)
+![alt img](./imgs/step2:auth-signup-mongoDB.png)
+The output
+![alt img](./imgs/step2:auth-signup-mongoDB-output.png)
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,38 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
-
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-fount-error";
+import mongoose from "mongoose";
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi There");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
 });
 
-app.listen(3000, () => {
-  console.log("Listening to port 3000!!!!!!!!!");
-});
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening to port 3000!!!!!!!!!");
+  });
+};
+
+start();
