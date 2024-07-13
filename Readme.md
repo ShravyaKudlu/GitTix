d69eb02d6f0343526b6cfe835ec71c09ef0725f3
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-07-13 14:01:00 -0400

    Client-nextJS-commit

--- Readme.md
+++ Readme.md
@@ -1,2 +1,13 @@
-This is my Auth Service Test Result
-![alt img](./imgs/testResul.png)
+What?
+Setting up Next.js with React to enhance my authentication service's user interaction.
+
+Challenge:
+Ensuring Next.js correctly interacts with an ingress-nginx setup, switching between server-side rendering (SSR) and its built-in route handler, especially when using the getInitialProps function.
+
+Resolution:
+Implemented Kubernetes namespaces to fetch the ingress namespace dynamically. Integrated this logic into the header component, enabling Next.js to seamlessly determine the routing strategy based on the environment. SSR improves performance and SEO by pre-rendering pages on the server, ensuring faster initial load times and enabling search engines to index content effectively, crucial for enhancing user experience and discoverability in my application.
+Pictures:
+![alt img](./imgs/resolution.png)
+
+This is my output:
+![alt img](./imgs/react-nextJS.gif)
---
+++ client/component/header.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li
+          key={href}
+          className="nav-item"
+        >
+          <Link
+            href={href}
+            passHref
+            legacyBehavior
+          >
+            <a
+              className="nav-link"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              {label}
+            </a>
+          </Link>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <Link
+        href="/"
+        className="navbar-brand"
+        style={{ color: "white" }}
+      >
+        GitTix
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
