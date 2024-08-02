8fb1a7f485478fa8197af7ad351652fd77577678
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-08-02 16:20:54 -0400

    complete project

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -17,9 +17,6 @@ export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
     if (!order) {
       throw new Error("Order not found");
     }
-    if (order.status === OrderStatus.Complete) {
-      return msg.ack();
-    }
     order.set({
       status: OrderStatus.Complete,
     });
